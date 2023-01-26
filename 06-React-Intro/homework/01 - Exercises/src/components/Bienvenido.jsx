4a17eae1a8d317d0ae44b4145dba3eea6ade4b3c
adriianzambrano <adrianx_13@hotmail.com>
2023-01-26 16:46:30 -0500

    Realizado test 1

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 
-const studentName = "";
+import Botones from "./Botones.jsx";
+const studentName = "Adrian Zambrano";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return(
+    <div>
+        <h1>Homework | 1 React</h1>
+        <h3>{studentName}</h3>
+        <ul>
+            {
+              techSkills.map((str, index)=><li key={index}>{str}</li>)
+            }
+        </ul>
+
+        <Botones alerts={alerts} />
+    </div>
+  )
 }
 
 // Esto lo exportamos para los tests
