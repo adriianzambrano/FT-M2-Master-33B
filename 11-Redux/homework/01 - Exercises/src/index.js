61dfa345decc8ee5f012a0171c4e9b9a2aeac56e
adriianzambrano <adrianx_13@hotmail.com>
2023-03-07 16:07:56 -0500

    Realizado

--- 11-Redux/homework/01 - Exercises/src/index.js
+++ 11-Redux/homework/01 - Exercises/src/index.js
@@ -1,24 +1,46 @@
 const { createStore } = require("redux");
 const contador = require("./reducer");
-const { incremento, decremento } = require("./actions");
+const { incremento, decremento, incrementoImpar, incrementoAsync } = require("./actions");
 
 // En esta línea creamos nuestro store. Pasándole como parámetro nuestro Reducer
-var store;
+var store = createStore(contador); 
+console.log('Store-> State', store.getState());
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor = document.getElementById("valor")
+//valor.innerHTML = 34
 
 // Esta función nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
 // Utilizamos el elemento obtenido arriba para mostrar el State.
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
+  var num = store.getState().contador
   // Seteamos el número obtenido como texto dentro del elemento con id 'valor':
+  valor.innerHTML = num 
 }
 
 // Ejecutamos la función 'renderContador':
+renderContador()
 
 // Nos subscribimos al store pasándole la misma función. Así cada vez que llegue una acción, ejecutamos la función:
+store.subscribe(() => {
+  renderContador()
+  console.log("store -> STATE ", store.getState());
+})
 
 // Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la acción correspondiente:
+ var btnIncrement = document.getElementById('incremento')
+ var btndecremento = document.getElementById('decremento')
+ var btnincrementoImpar = document.getElementById('incrementoImpar')
+ var btnIncrementoAsync = document.getElementById('incrementoAsync')
+
+ btnIncrement.onclick = ()=> store.dispatch( incremento("Adrian"))
+ btndecremento.onclick = ()=> store.dispatch( decremento())
+ btnincrementoImpar.onclick = ()=> store.dispatch( incrementoImpar())
+ btnIncrementoAsync.onclick = ()=> {
+  setTimeout(()=>{
+    store.dispatch( incrementoAsync());
+  }, 2000)
+ }
--- 11-Redux/homework/01 - Exercises/src/reducer/index.js
+++ 11-Redux/homework/01 - Exercises/src/reducer/index.js
@@ -1,15 +1,48 @@
-const { INCREMENTO, DECREMENTO } = require('../action-types');
+const { INCREMENTO, DECREMENTO, INCREMENTOIMPAR, INCREMENTOASYNC } = require('../action-types');
 
 const initialState = {
-  contador: 0
+  contador: 0,
+  names: []
 }
 
 // Nuestro reducer que maneja nuestros dos casos de acción incremento y decremento.
 // Recibe el estado de nuestro store, junto con una action creada por nuestro action creator. 
 // ¿Qué tiene que hacer el reducer con el contador de cada caso?
 
+// redux toma el objeto -------------> {}
 function contador(state = initialState, action) {
-  
-}
+
+  switch (action.type) {
+    case INCREMENTO:
+      return{
+        ...state,
+        contador: state.contador + 1,
+        names: [...state.names, action.payload ]
+      }
+    case DECREMENTO:
+      return{
+        ...state,
+        contador: state.contador - 1
+      }
+    case INCREMENTOIMPAR:
+      if (state.contador % 2 !== 0) {
+        return{
+          ...state,
+          contador: state.contador + 2
+        }
+      }else{
+        return {
+          ...state
+        }
+      }
+    case INCREMENTOASYNC:
+      return{
+        ...state,
+        contador: state.contador + 10
+      }
+    default:
+      return state
+  }
+} 
 
 module.exports = contador;
